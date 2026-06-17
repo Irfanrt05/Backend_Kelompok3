6a1ecf80c8feb803b64bc0598e36736c0ca3077e
Arif Rahman Hakim <burninglegion11@gmail.com>
2026-06-17 09:12:29 +0700

    fixed favorite

--- models/favorite.js
+++ models/favorite.js
@@ -3,13 +3,12 @@ import sequelize from "../config/database.js";
 
 const Favorite = sequelize.define("favorites", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  user_id: { type: DataTypes.INTEGER, allowNull: false },
-  recipe_id: { type: DataTypes.INTEGER, allowNull: false },
+  user_id: { type: DataTypes.INTEGER, allowNull: true },
+  recipe_id: { type: DataTypes.INTEGER, allowNull: true },
   blog_id: { type: DataTypes.INTEGER, allowNull: true },
 }, {
   underscored: true,
   timestamps: true,
   indexes: [{ unique: true, fields: ["user_id", "recipe_id"] }, { unique: true, fields: ["user_id", "blog_id"] }]
 });
-
 export default Favorite;
